14f2b2e2f232393abf5b1a7d4a8fc0f8da411bd3
Kaveena <Kaveena>
2016-03-27 16:16:11 +0200

    declaration des types

--- type.hpp
+++ type.hpp
@@ -5,8 +5,18 @@
 #include <mraa/gpio.h>
 
 
-class analog {};
-class digital {};
+class analog {
+protected:
+  float valeur;//peut etre une entree ou une sortie dependant de la deuxieme classe associee
+public:
+  virtual float get_val();
+};
+class digital {
+protected:
+  bool valeur;
+public:
+  virtual bool get_val();
+};
 
 #endif
 
